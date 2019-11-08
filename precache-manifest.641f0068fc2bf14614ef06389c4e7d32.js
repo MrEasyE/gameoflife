8aa5f09189165556ce3d95c3af482ee70308ec1e
_MrEasyE_ <mreasye@protonmail.com>
2019-11-08 18:16:18 -0500

    Updates

---
+++ precache-manifest.641f0068fc2bf14614ef06389c4e7d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2a7b2fe0bbc12ada856eb1e7b8502b",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "c93f7c5ba81040429cf2",
+    "url": "/gameoflife/static/css/main.09cb7f15.chunk.css"
+  },
+  {
+    "revision": "b0324c06abe9a4352cbd",
+    "url": "/gameoflife/static/js/2.1f191105.chunk.js"
+  },
+  {
+    "revision": "c93f7c5ba81040429cf2",
+    "url": "/gameoflife/static/js/main.23d4cbe6.chunk.js"
+  },
+  {
+    "revision": "770782f105f07c04b959",
+    "url": "/gameoflife/static/js/runtime-main.34747759.js"
+  }
+]);
